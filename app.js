b36156f90fda3de1cee98cb6cc268024d5219c35
danielle.araujo <danielle.araujo@aluno.edu.pi.gov.br>
2026-06-16 19:04:57 +0000

    criei um app.js

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const calcudora=require (./calcudora.js')
-    let resultado soma=calcudora.somar (1,2);
-    console.log (resultado soma);
+const calcudora=require ('./calcudora.js')
+    let resultadosoma=calcudora.soma(1,2);
+    console.log (resultadosoma);
+
+    const calcudora=require ('./calcudora.js')
+    let resultadosubtrair=calcudora.subtrair(1,2)
+    console.log (resultadosubtrair);
+    
+    const calculadora=require ('.calculadora.js')
+     let resultadomultiplicar=calculadora.multiplicar (1,2)
+     console.log (resultadomultiplicar);
+
+     const calcudora=require ('.calculadora.js')
+     let resultadodivisão=calcudora.dividir(1,2)
+     console.log (resultadodivisão)
 
