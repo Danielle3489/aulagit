756d8e0e1cb1c9b01013608efe089c8903afd8b2
danielle.araujo <danielle.araujo@aluno.edu.pi.gov.br>
2026-06-16 19:24:05 +0000

    crei um objeto em js

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const pessoa={
+    nome:"Danielle",
+    idade:17,
+    peso:55,
+    altura:1.58,
+    mesnascimento:13.12
+}
+    console.log(pessoa.nome);
+    pessoa.idade=17
+    console.log (pessoa.altura)
+    console.log(pessoa.mesnascimento)
